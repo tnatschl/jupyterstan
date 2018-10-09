b35b0e70a6982f594f8fac913b6cec9e6879a978
Jan Freyberg <jan.freyberg@gmail.com>
2018-10-09 13:44:16 +0100

    Version bump for timing

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ readme = open("README.md").read()
 
 setup(
     name="jupyterstan",
-    version="0.1.2",
+    version="0.1.3",
     description="Magics for defining stan code in notebooks.",
     long_description=readme,
     long_description_content_type="text/markdown",
